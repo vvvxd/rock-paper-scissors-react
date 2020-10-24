0862f43abd1d011da0c621410a6f934429bb86c4
Valentin <qweqweq@qwe>
2020-10-24 19:00:01 +0300

    Сделал обновление счета

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
 
-import rock from '../../images/icon-rock.svg'
-import paper from '../../images/icon-paper.svg'
-import scissors from '../../images/icon-scissors.svg'
+import rock from '../../images/icon-rock.svg';
+import paper from '../../images/icon-paper.svg';
+import scissors from '../../images/icon-scissors.svg';
 
-import './Items.scss'
+import './Items.scss';
+import { Link } from 'react-router-dom';
 
-const Items = ({setActiveItem,history}) => {
-    return (
-        <div className='game__items'>
-            <div
-                onClick={()=>{
-                    history.push('/game');
-                    setActiveItem('paper');
-                }}
-                className="game__items-paper"
-            >
-                <img src={paper} alt=""/>
-            </div>
-            <div
-                onClick={()=>{
-                    history.push('/game');
-                    setActiveItem('rock');
-                }}
-                className="game__items-rock"
-            >
-                <img src={rock} alt=""/>
-            </div>
-            <div
-                onClick={()=>{
-                    history.push('/game');
-                    setActiveItem('scissors');
-                }}
-                className="game__items-scissors"
-            >
-                <img src={scissors} alt=""/>
-            </div>
-        </div>
-    )
-    ;
+const Items = ({ setActiveItem, setRandom }) => {
+  return (
+    <div className="game__items">
+      <Link
+        to="/game"
+        onClick={() => {
+          setActiveItem('paper');
+          setRandom();
+        }}
+        className="game__items-paper">
+        <img src={paper} alt="" />
+      </Link>
+      <Link
+        to="/game"
+        onClick={() => {
+          setActiveItem('rock');
+          setRandom();
+        }}
+        className="game__items-rock">
+        <img src={rock} alt="" />
+      </Link>
+      <Link
+        to="/game"
+        onClick={() => {
+          setActiveItem('scissors');
+          setRandom();
+        }}
+        className="game__items-scissors">
+        <img src={scissors} alt="" />
+      </Link>
+    </div>
+  );
 };
 
 export default Items;
--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 
-import './Score.scss'
-import logo from '../../images/logo.svg'
+import './Score.scss';
+import logo from '../../images/logo.svg';
 
-
-const Score = ({score}) => {
-    return (
-        <div className="game__score">
-            <img src={logo} alt="qwe"/>
-            <div className="game__score-num">
-                <span>
-                    score
-                </span>
-                <strong>
-                    {score}
-                </strong>
-            </div>
-        </div>
-    );
+const Score = ({ score }) => {
+  return (
+    <div className="game__score">
+      <img src={logo} alt="qwe" />
+      <div className="game__score-num">
+        <span>score</span>
+        <strong>{score}</strong>
+      </div>
+    </div>
+  );
 };
 
 export default Score;
